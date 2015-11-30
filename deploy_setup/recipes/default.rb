92809839686b486ee21c34c3343ff7fbf654ad96
Janus <janus.zhou1005@gmail.com>
2015-11-30 12:29:09 -0500

    Correct symbolic link

--- deploy_setup/recipes/default.rb
+++ deploy_setup/recipes/default.rb
@@ -1,7 +1,7 @@
 Chef::Log.info("******Creating a data directory.******")
 node[:deploy].each do |application, deploy|
-  link '/srv/www/skyphp' do
-    to '/var/www/skyphp'
+  link '/var/www/skyphp' do
+    to '/srv/www/skyphp'
     link_type :symbolic
   end
 end
