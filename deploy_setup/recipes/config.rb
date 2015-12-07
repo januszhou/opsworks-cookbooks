8efb7de2120f18dd5a3cf0a85816d272ab5e9bfb
Janus <janus.zhou1005@gmail.com>
2015-12-07 17:35:33 -0500

    Fix case issue

--- deploy_setup/recipes/config.rb
+++ deploy_setup/recipes/config.rb
@@ -3,7 +3,7 @@ node[:deploy].each do |application, deploy|
   execute "echo 'display_errors = On' >> /etc/php.ini"
   execute "echo 'error_reporting = E_ERROR & ~E_DEPRECATED & ~E_STRICT' >> /etc/php.ini"
   # start apache
-  execute "grep -rl 'AllowOverride None' /etc/httpd/conf/httpd.conf | xargs sed -i 's/AllowOverride None/AllowOverride All/g'"
+  execute "grep -rl 'AllowOverride none' /etc/httpd/conf/httpd.conf | xargs sed -i 's/AllowOverride none/AllowOverride All/g'"
   execute 'sudo service httpd start'
   execute 'sudo service httpd restart'
 end
