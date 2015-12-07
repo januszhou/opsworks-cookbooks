fb40384e2f83d451c7cea2acd911e4c33cde1820
Janus <janus.zhou1005@gmail.com>
2015-12-07 17:04:07 -0500

    Enable allow override

--- deploy_setup/recipes/config.rb
+++ deploy_setup/recipes/config.rb
@@ -3,6 +3,7 @@ node[:deploy].each do |application, deploy|
   execute "echo 'display_errors = On' >> /etc/php.ini"
   execute "echo 'error_reporting = E_ERROR & ~E_DEPRECATED & ~E_STRICT' >> /etc/php.ini"
   # start apache
-  execute 'sudo apachectl start'
-  execute 'sudo apachectl restart'
+  execute 'grep -rl 'AllowOverride None' /etc/httpd/conf/httpd.conf | xargs sed -i 's/AllowOverride None/AllowOverride All/g''
+  execute 'sudo service httpd start'
+  execute 'sudo service httpd restart'
 end
