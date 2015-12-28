7b43a64ca24c9969396e6db99cf219f305b2b4b2
Janus <janus.zhou1005@gmail.com>
2015-12-28 12:37:56 -0500

    Still have to use relative path

--- deploy_setup/recipes/node.rb
+++ deploy_setup/recipes/node.rb
@@ -1,9 +1,12 @@
 node[:deploy].each do |application, deploy|
 	base = "/srv/www/carve_cart/current"
 
-	execute "cd #{base}"
+	directory "#{base}/current"
+
+	execute "cd #{base}/current"
+
 	# install bower dependencies
-	execute "node_modules/bower/bin/bower install --allow-root"
+	execute "#{base}/node_modules/bower/bin/bower install --allow-root"
 
 	# link up into public
 	link "public/bower_components" do
