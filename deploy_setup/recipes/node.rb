56fffa6ba8330ae7c622ca0d6e4981eac31a736e
Janus <janus.zhou1005@gmail.com>
2015-12-28 11:46:56 -0500

    Setup bower allow root

--- deploy_setup/recipes/node.rb
+++ deploy_setup/recipes/node.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |application, deploy|
 	base = "/srv/www/carve_cart/current"
 
 	# install bower dependencies
-	execute "#{base}/node_modules/bower/bin/bower install"
+	execute "#{base}/node_modules/bower/bin/bower install --allow-root"
 
 	# link up into public
 	link "#{base}/public/bower_components" do
