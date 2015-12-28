3ec4f06197af4a6920b4af8bc6a2082d5cd3815d
Janus <janus.zhou1005@gmail.com>
2015-12-28 11:01:37 -0500

    Node application deploy setup

---
+++ deploy_setup/recipes/node.rb
@@ -0,0 +1,12 @@
+node[:deploy].each do |application, deploy|
+	base = node["deploy"]["appshortname"]["deploy_to"]
+	directory "#{base}/current"
+
+	# install bower dependencies
+	execute "node_modules/bower/bin/bower install"
+
+	# link up into public
+	link "public/bower_components" do
+		to "bower_components"
+	end
+end
