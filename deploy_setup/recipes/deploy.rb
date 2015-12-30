eb6f65a5c3eb7f0b19027f50cca9b86d15a7f2ba
JANUS ZHOU <janus.zhou1005@gmail.com>
2015-12-30 10:14:02 -0500

    Remove checkout

--- deploy_setup/recipes/deploy.rb
+++ deploy_setup/recipes/deploy.rb
@@ -30,7 +30,6 @@ node[:deploy].each do |application, deploy|
         repository fullLists[folder]['url']
         checkout_branch fullLists[folder]['branch']
         revision fullLists[folder]['branch']
-        enable_checkout false
         action :sync
         ssh_wrapper "/root/git_wrapper.sh"
       end
